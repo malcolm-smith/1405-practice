38eede67f8249fea8a0e6581bcc75bbafaafbe05
helloMusa <esquitoban@gmail.com>
2019-10-18 15:36:59 -0400

    Added solution for pm2p4

--- midterm-practice/02/problem-04/musa-ali/problem4.py
+++ midterm-practice/02/problem-04/musa-ali/problem4.py
@@ -1,18 +1,35 @@
-x = 20
-d1 = []
-d2 = []
+# Practice Midterm 2
+# Greatest Common Divisor
 
-for i in range(1, 100):
-	if 20 % i == 0:
-		d1.append(i)
-	if 24 % i == 0:
-		d2.append(i)
+import math
 
-for i in range(1, 100):
-	if d1[i] == d2[i]:
-		print(d2[i])
+# Returns the greatest common divisor between two integers
+def gcd(a, b):
+	if a < 1 or b < 1:
+		return -1
 
+	divs_a = []
+	for i in range(1, a + 1):
+		if a % i == 0:
+			divs_a.append(i)
 
-print(d1)
-print(d2)
-# print(y)
+	divs_b = []
+	for i in range(1, b + 1):
+		if b % i == 0:
+			divs_b.append(i)
+
+	common = []
+	for num in divs_a:
+		if num in divs_b:
+			common.append(num)
+
+	return max(common)
+
+def main():
+	a = int(input("Enter first number: "))
+	b = int(input("Enter second number: "))
+	gcd_ab = gcd(a, b)
+	print("The Greatest Common Divisor between " + str(a) + " and " + str(b) + " is " + str(gcd_ab))
+
+if __name__ == '__main__':
+	main()
