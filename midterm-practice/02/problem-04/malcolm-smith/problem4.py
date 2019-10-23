75bbb1eee965bb2b33fc5ea1a653ce1e96d2f91e
malcolm-smith <malcolm.henry.smith@gmail.com>
2019-10-23 00:56:21 -0400

    added solution for case a,b < 0

--- midterm-practice/02/problem-04/malcolm-smith/problem4.py
+++ midterm-practice/02/problem-04/malcolm-smith/problem4.py
@@ -1,11 +1,18 @@
 # finds GCD using Euclid's Algorithm (https://en.wikipedia.org/wiki/Euclidean_algorithm)
 # is recursive (https://en.wikipedia.org/wiki/Recursion_(computer_science))
-def gcd(a, b):
+def gcd_euclid(a, b):
     if a % b == 0:
         return b
     if b > a:
-        return gcd(b, a)
-    return gcd(b, a % b)
+        return gcd_euclid(b, a)
+    return gcd_euclid(b, a % b)
+
+# GCD will always be -1 if one number is < 1
+def gcd(a, b):
+    if a < 1 or b < 1:
+        return -1
+    return gcd_euclid(a, b)
 
 
 print(gcd(20, 24))
+print(gcd(-6, 0))
