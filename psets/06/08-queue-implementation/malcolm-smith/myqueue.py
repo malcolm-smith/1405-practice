78bb55bd1cfc5ae8a93dc65db8e09b3e7661bdb8
malcolm-smith <malcolm.henry.smith@gmail.com>
2019-11-18 14:51:26 -0500

    formatting

--- psets/06/08-queue-implementation/malcolm-smith/myqueue.py
+++ psets/06/08-queue-implementation/malcolm-smith/myqueue.py
@@ -1,29 +1,35 @@
 maxsize = 10
 
+
 def enqueue(queue, value):
     if len(queue) < maxsize:
         queue.append(value)
         return True
     return False
 
+
 def dequeue(queue):
     if len(queue):
         return queue.pop(0)
     return None
+
 
 def peek(queue):
     if len(queue):
         return queue[0]
     return None
 
+
 def isempty(queue):
     return len(queue) == 0
+
 
 def multienqueue(queue, item):
     count = 0
     while enqueue(queue, item.pop(0)):
         count += 1
     return count
+
 
 def multidequeue(queue, number):
     removed = []
